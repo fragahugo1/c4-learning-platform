75ee805dc4c376b2d9eac916e89987db2662fb88
Felipinho1806 <fcnfelipe04@gmail.com>
2026-03-21 19:40:41 -0300

    Criação do componente para a barra horizontal

---
+++ frontend/src/app/horizontal-bar/horizontal-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-bar',
+  imports: [],
+  templateUrl: './horizontal-bar.html',
+  styleUrl: './horizontal-bar.css',
+})
+export class HorizontalBar {
+
+}
