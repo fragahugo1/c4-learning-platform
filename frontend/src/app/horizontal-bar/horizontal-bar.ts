b2ff242cfd4455ce58423c5d821eae6aa344cc99
Felipinho1806 <fcnfelipe04@gmail.com>
2026-03-21 21:57:55 -0300

    Estilização de como será a barra horizontal

--- frontend/src/app/horizontal-bar/horizontal-bar.ts
+++ frontend/src/app/horizontal-bar/horizontal-bar.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-horizontal-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './horizontal-bar.html',
-  styleUrl: './horizontal-bar.css',
+  styleUrls: ['./horizontal-bar.css'],
 })
 export class HorizontalBar {
+  selectedTool = 'select';
 
+  tools = [
+    { icon: 'pi pi-pencil', value: 'draw' },
+    { icon: 'pi pi-comment', value: 'text' },
+    { icon: 'pi pi-trash', value: 'layer' },
+    { icon: 'pi pi-palette', value: 'layer' }
+  ];
+
+  selectTool(tool: any) {
+    this.selectedTool = tool.value;
+  }
 }
