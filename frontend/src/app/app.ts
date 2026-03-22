aae3c1a2ef62cbd92f7aa7948a4ccf12f5872c9b
Felipinho1806 <fcnfelipe04@gmail.com>
2026-03-21 21:58:12 -0300

    Implementação da barra na tela principal

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DrawerModule } from 'primeng/drawer';
 import { Sidebar } from './sidebar/sidebar';
+import { HorizontalBar } from './horizontal-bar/horizontal-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DrawerModule, Sidebar],
+  imports: [RouterOutlet, DrawerModule, Sidebar, HorizontalBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
