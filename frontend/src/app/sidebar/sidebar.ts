310d91348abc4fac81f3a49310806b655097bff4
Hugo Fraga <hugoxfraga@hotmail.com>
2026-03-30 23:36:23 -0300

    Introduzindo os elementos no sidebar

--- frontend/src/app/sidebar/sidebar.ts
+++ frontend/src/app/sidebar/sidebar.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DrawerModule, ButtonModule],
+  imports: [DrawerModule, ButtonModule, DragDropModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   sidebarVisible: boolean = false;
+
+  onDragStart(event: DragEvent, type: string) {
+    if (event.dataTransfer) {
+      // Guardamos o tipo de elemento (ex: 'Rectangle') dentro do evento de arrasto
+      event.dataTransfer.setData('type', type);
+      
+      // Define o efeito visual do mouse
+      event.dataTransfer.effectAllowed = 'copy';
+    }
+  }
 }
