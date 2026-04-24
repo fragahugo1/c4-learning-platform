30b0556da4674e03f3192934f3145feeef2daeab
fragahugo1 <hugoxfraga@hotmail.com>
2026-04-24 12:16:40 -0300

    adaptações nos elementos e estilizações da sidebar para suportar esse minimundo

--- frontend/src/app/sidebar/sidebar.ts
+++ frontend/src/app/sidebar/sidebar.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { MiniMundoService } from '../services/mini-mundo.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DrawerModule, ButtonModule, DragDropModule],
+  imports: [DrawerModule, ButtonModule, DragDropModule, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  currentLayer = input<number>(1);
   sidebarVisible: boolean = false;
+
+  currentPhase = computed(() => {
+    return this.miniMundoService.getPhaseByLayer(this.currentLayer());
+  });
+
+  constructor(private miniMundoService: MiniMundoService) {}
 
   onDragStart(event: DragEvent, type: string) {
     if (event.dataTransfer) {
